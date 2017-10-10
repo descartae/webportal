433fe2c279d12840aff77f416df016d9f22c4547
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-10 07:06:28 -0700

    set up server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import {
+  graphqlExpress,
+  graphiqlExpress,
+} from 'graphql-server-express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import { schema } from './src/schema';
+
+const PORT = 4000;
+const server = express();
+
+server.use('*', cors({ origin: 'http://localhost:3000' }));
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
+
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+
+server.listen(PORT, () =>
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
+);
