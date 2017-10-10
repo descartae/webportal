2260e0d398d0e2e5e0509690ad3ef985eb9ac21e
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-10 07:27:13 -0700

    reorganize code

--- client/src/components/CenterListing.js
+++ client/src/components/CenterListing.js
@@ -1,17 +1,35 @@
 import React from 'react'
-import './CenterListing.css'
-
 import { gql, graphql } from 'react-apollo'
 
-const centersListQuery = gql`
-  query CentersQuery {
+const CenterListing = ({ data: { loading, error, centers } }) => {
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>{ error.message }</p>
+  }
+
+  return (
+    <div className="centersList">
+      { centers.map( it =>
+        (<div key={it.id} className="center">{it.name}</div>)
+      )}
+    </div>
+
+  )
+}
+
+export const centersListQuery = gql`
+  query CentersListQuery {
     centers {
       id
       name
     }
   }
-`
+`;
 
+/*
 const CenterListing = ({ data: { loading, error, centers } }) => {
   if (loading) {
     return <p>Loading...</p>
@@ -27,6 +45,8 @@ const CenterListing = ({ data: { loading, error, centers } }) => {
     </ul>
   )
 }
+ */
 
-export { centersListQuery, CenterListing }
-export const CenterListingWithData = graphql(centersListQuery)(CenterListing)
+export { CenterListing }
+export const CenterListingWithData = graphql(centersListQuery)(CenterListing) 
+
