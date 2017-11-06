4e4836ef7715f583082216cc049c7137b37919e3
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-11-05 16:02:19 -0800

    find one center query update

--- server/src/schema/center/queries.js
+++ server/src/schema/center/queries.js
@@ -1,7 +1,14 @@
+import {ObjectID} from 'mongodb';
+
 export const centers = (obj, args, { collections: { Centers } }, info) => {
   return Centers.find().toArray()
 }
-
+/*
 export const center = (obj, { _id }, { dataLoaders: { Centers } }, info) => {
   return Centers.load(_id)
+}
+*/
+// Find one center by ID
+export const center = (obj, args, { collections: { Centers } }, info) => {
+  return Centers.findOne({_id: ObjectID(args._id) })
 }
