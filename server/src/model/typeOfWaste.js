267e85ad1f53e854b21de66a9bfa3b50822f99b3
Eduardo Pereira <eduardopacunha@gmail.com>
2017-11-07 12:44:37 -0200

    Add again the per-connection dataloader for type of waste queries

--- server/src/model/typeOfWaste.js
+++ server/src/model/typeOfWaste.js
@@ -1,64 +1,76 @@
 import { assertNotEmpty } from './validation'
+import DataLoader from 'dataloader'
 
-export default ({ TypesOfWaste }) => ({
-  // Root queries
-  async typeOfWaste (_id) {
-    return TypesOfWaste.findOne({ _id })
-  },
-  async typesOfWaste () {
-    return TypesOfWaste.find({ enabled: true }).toArray()
-  },
-  // Operations
-  async addTypeOfWaste ({ name, icon }) {
-    assertNotEmpty(name, 'name')
-    assertNotEmpty(icon, 'icon')
+export default ({ TypesOfWaste }) => {
+  const batcher =
+    async (ids) => TypesOfWaste.find({ _id: { $in: ids } }).toArray()
 
-    const item = {
-      name,
-      icon,
-      enabled: true
-    }
+  const options = {
+    cacheKeyFn: (key) => key.toString()
+  }
 
-    const { ops: [result] } = await TypesOfWaste.insert(item)
+  const dataloader = new DataLoader(batcher, options)
 
-    return {
-      success: true,
-      typeOfWaste: result
-    }
-  },
-  async updateTypeOfWaste ({ _id, patch: { name, icon } }) {
-    const update = {}
+  return {
+    // Root queries
+    async typeOfWaste (_id) {
+      return dataloader.load(_id)
+    },
+    async typesOfWaste () {
+      return TypesOfWaste.find({ enabled: true }).toArray()
+    },
+    // Operations
+    async addTypeOfWaste ({ name, icon }) {
+      assertNotEmpty(name, 'name')
+      assertNotEmpty(icon, 'icon')
 
-    if (name != null) {
-      update.name = name
-    }
-
-    if (icon != null) {
-      update.icon = icon
-    }
-
-    const { value } =
-      await TypesOfWaste.findOneAndUpdate({
-        _id,
+      const item = {
+        name,
+        icon,
         enabled: true
-      }, {
-        $set: update
-      }, {
-        returnOriginal: false
-      })
+      }
 
-    return {
-      success: true,
-      typeOfWaste: value
-    }
-  },
-  async disableTypeOfWaste ({ _id }) {
-    const update = { enabled: false }
+      const { ops: [result] } = await TypesOfWaste.insert(item)
 
-    await TypesOfWaste.updateOne({ _id }, { $set: update })
+      return {
+        success: true,
+        typeOfWaste: result
+      }
+    },
+    async updateTypeOfWaste ({ _id, patch: { name, icon } }) {
+      const update = {}
 
-    return {
-      success: true
+      if (name != null) {
+        update.name = name
+      }
+
+      if (icon != null) {
+        update.icon = icon
+      }
+
+      const { value } =
+        await TypesOfWaste.findOneAndUpdate({
+          _id,
+          enabled: true
+        }, {
+          $set: update
+        }, {
+          returnOriginal: false
+        })
+
+      return {
+        success: true,
+        typeOfWaste: value
+      }
+    },
+    async disableTypeOfWaste ({ _id }) {
+      const update = { enabled: false }
+
+      await TypesOfWaste.updateOne({ _id }, { $set: update })
+
+      return {
+        success: true
+      }
     }
   }
-})
+}
