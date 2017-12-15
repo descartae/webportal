a0aeffba14840c6ff76dce2945c13a691b1d0ac0
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-12-14 17:17:22 -0800

    update styling

--- client/src/components/FacilityDetails.js
+++ client/src/components/FacilityDetails.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { propType } from 'graphql-anywhere';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import ActionRoom from 'material-ui/svg-icons/action/room';
+import CommunicationCall from 'material-ui/svg-icons/communication/call';
+import MapsLocalOffer from 'material-ui/svg-icons/maps/local-offer';
+import ActionSchedule from 'material-ui/svg-icons/action/schedule';
 
 export default class FacilityDetails extends React.Component {
 
@@ -39,27 +51,78 @@ export default class FacilityDetails extends React.Component {
   }
 
   render () {
+
+    const state = {
+      fixedHeader: true,
+      stripedRows: false,      
+      showRowHover: false,       
+      selectable: false,   
+      multiSelectable: false,     
+      enableSelectAll: false,
+      deselectOnClickaway: false,    
+      showCheckboxes: false, 
+      height: '300px',    
+    };
+
     return (
-      <div className='w-100 pa4 flex justify-center'>
-        <div style={{ maxWidth: 400 }}>
-          <input
-            className='w-100 pa3 mv2'
-            value={this.props.facility.name}
-            placeholder='Name'
-            readOnly={true}
-          />
-          <input
-            className='w-100 pa3 mv2'
-            value={this.props.facility.location.address}
-            placeholder='Address'
-            readOnly={true}
-          />
-          }
+<div className='container'>
+      <div className='facilityDetails'>
+        <h3>{this.props.facility.name}</h3>
+        <hr/>
+        <p><ActionRoom /> Endereço: {this.props.facility.location.address}, {this.props.facility.location.municipality}, {this.props.facility.location.state} {this.props.facility.location.zip}</p>
+        <p><CommunicationCall/> Contato: {this.props.facility.telephone}</p>
+        <p><strong>Tipo de resíduo</strong>:
+        { this.props.facility.typesOfWaste.map(it => (
+          <p key={it._id} className='typesOfWastes'>
+            {/* image fails to load
+            <img src={it.icon} alt={it.name} /> */}
+           <MapsLocalOffer/> {it.name}
+          </p>
+        ))}
+        </p> 
+        <br/>
+        <Table
+          header={state.height}
+          fixedHeader={state.fixedHeader}
+          selectable={state.selectable} 
+          multiSelectable={state.multiSelectable}
+        >
+          <TableHeader
+            displaySelectAll={state.showCheckboxes}
+            adjustForCheckbox={state.showCheckboxes}
+            enableSelectAll={state.enableSelectAll}
+          >
+            <TableRow>
+              <TableHeaderColumn colSpan="3" style={{textAlign: 'left'}}>
+                <ActionSchedule/> Horários
+              </TableHeaderColumn>
+            </TableRow>
+            <TableRow>
+              <TableHeaderColumn>Dias</TableHeaderColumn>
+              <TableHeaderColumn>Começar</TableHeaderColumn>
+              <TableHeaderColumn>Fim</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>         
+          <TableBody
+            displayRowCheckbox={state.showCheckboxes}
+            deselectOnClickaway={state.deselectOnClickaway}
+            showRowHover={state.showRowHover}
+            stripedRows={state.stripedRows}
+          >
+            {this.props.facility.openHours.map(it => (
+              <TableRow key={it.dayOfWeek}>
+                <TableRowColumn>{it.dayOfWeek}</TableRowColumn>
+                <TableRowColumn>{it.startTime}:00</TableRowColumn>
+                <TableRowColumn>{it.endTime}:00</TableRowColumn>
+              </TableRow>
+              ))}
+          </TableBody>
           <div className='flex justify-between'>
             <button onClick={this.props.handleCancel}>Cancel</button>
           </div>
-        </div>
+        </Table>
       </div>
+    </div>
     )
   }
 }; 
