9c7e9b64a5bf0ba7c83c48fd648d4870c05471a6
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-12-14 16:44:50 -0800

    switch FacilityPage with FacilityDetails

--- client/src/components/FacilityDetails.js
+++ client/src/components/FacilityDetails.js
@@ -1,64 +1,65 @@
 import React from 'react';
-import NotFound from './NotFound';
-import {
-    gql,
-    graphql,
-} from 'react-apollo';
+import { propType } from 'graphql-anywhere';
+import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
-import { filter } from 'graphql-anywhere';
-import { withRouter } from 'react-router-dom';
-import FacilityPage from './FacilityPage';
 
-class FacilityDetails extends React.Component {
+export default class FacilityDetails extends React.Component {
 
-  static propTypes = {
-    data: PropTypes.shape({
-      loading: PropTypes.bool,
-      error: PropTypes.object,
-      Facility: PropTypes.object,
-    }).isRequired,
-    history: PropTypes.object.isRequired
+
+  static fragments = {
+    facility: gql`
+      fragment FacilityDetailsFacility on Facility {
+            _id
+            name
+            telephone
+            website
+            location {
+              address
+              municipality
+              state
+              zip
+            }
+            typesOfWaste {
+              _id
+              name
+              icon
+            }
+            openHours {
+              dayOfWeek
+              startTime
+              endTime
+            }
+          }
+    `
   }
 
+  static propTypes = {
+    facility: propType(FacilityDetails.fragments.facility).isRequired,
+    handleCancel: PropTypes.func.isRequired,
+  }
 
   render () {
-    console.log(this.props.data)
-    if (this.props.data.loading) {
-      return (<div>Loading</div>)
-    }
-
-    if (this.props.data.error) {
-      console.log(this.props.data.error)
-      return (<div>An unexpected error occurred</div>)
-    }
-
-    const facility = this.props.data.facility
-
     return (
-      <div>
-        <FacilityPage facility={filter(FacilityPage.fragments.facility, facility)} handleCancel={this.goBack}/>
+      <div className='w-100 pa4 flex justify-center'>
+        <div style={{ maxWidth: 400 }}>
+          <input
+            className='w-100 pa3 mv2'
+            value={this.props.facility.name}
+            placeholder='Name'
+            readOnly={true}
+          />
+          <input
+            className='w-100 pa3 mv2'
+            value={this.props.facility.location.address}
+            placeholder='Address'
+            readOnly={true}
+          />
+          }
+          <div className='flex justify-between'>
+            <button onClick={this.props.handleCancel}>Cancel</button>
+          </div>
+        </div>
       </div>
     )
   }
-
-  goBack = () => {
-    this.props.history.push('/');
-  }
-}
-
-
-const facilityDetailsQuery = gql`
-  query FacilityDetailsQuery($facilityId: ID!) {
-  facility(_id: $facilityId) {
-      ... FacilityPageFacility
-    }
-  }
-  ${FacilityPage.fragments.facility}
-`;
-
-export { FacilityDetails }
-export const FacilityDetailsWithData = graphql(facilityDetailsQuery, {
-  options: (props) => ({
-    variables: { facilityId: props.match.params.facilityId}
-  })
-})(withRouter(FacilityDetails));
+}; 
--- client/src/components/FacilityPage.js
+++ client/src/components/FacilityPage.js
@@ -1,65 +1,64 @@
 import React from 'react';
-import { propType } from 'graphql-anywhere';
-import gql from 'graphql-tag';
+import NotFound from './NotFound';
+import {
+    gql,
+    graphql,
+} from 'react-apollo';
 import PropTypes from 'prop-types';
+import { filter } from 'graphql-anywhere';
+import { withRouter } from 'react-router-dom';
+import FacilityDetails from './FacilityDetails';
 
-export default class FacilityPage extends React.Component {
-
-
-  static fragments = {
-    facility: gql`
-      fragment FacilityPageFacility on Facility {
-            _id
-            name
-            telephone
-            website
-            location {
-              address
-              municipality
-              state
-              zip
-            }
-            typesOfWaste {
-              _id
-              name
-              icon
-            }
-            openHours {
-              dayOfWeek
-              startTime
-              endTime
-            }
-          }
-    `
-  }
+class FacilityPage extends React.Component {
 
   static propTypes = {
-    facility: propType(FacilityPage.fragments.facility).isRequired,
-    handleCancel: PropTypes.func.isRequired,
+    data: PropTypes.shape({
+      loading: PropTypes.bool,
+      error: PropTypes.object,
+      Facility: PropTypes.object,
+    }).isRequired,
+    history: PropTypes.object.isRequired
   }
 
+
   render () {
+    console.log(this.props.data)
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+
+    if (this.props.data.error) {
+      console.log(this.props.data.error)
+      return (<div>An unexpected error occurred</div>)
+    }
+
+    const facility = this.props.data.facility
+
     return (
-      <div className='w-100 pa4 flex justify-center'>
-        <div style={{ maxWidth: 400 }}>
-          <input
-            className='w-100 pa3 mv2'
-            value={this.props.facility.name}
-            placeholder='Name'
-            readOnly={true}
-          />
-          <input
-            className='w-100 pa3 mv2'
-            value={this.props.facility.location.address}
-            placeholder='Image Url'
-            readOnly={true}
-          />
-          }
-          <div className='flex justify-between'>
-            <button onClick={this.props.handleCancel}>Cancel</button>
-          </div>
-        </div>
+      <div>
+        <FacilityDetails facility={filter(FacilityDetails.fragments.facility, facility)} handleCancel={this.goBack}/>
       </div>
     )
   }
-}; 
+
+  goBack = () => {
+    this.props.history.push('/');
+  }
+}
+
+
+const facilityPageQuery = gql`
+  query FacilityPageQuery($facilityId: ID!) {
+  facility(_id: $facilityId) {
+      ... FacilityDetailsFacility
+    }
+  }
+  ${FacilityDetails.fragments.facility}
+`;
+
+export { FacilityPage }
+export const FacilityPageWithData = graphql(facilityPageQuery, {
+  options: (props) => ({
+    variables: { facilityId: props.match.params.facilityId}
+  })
+})(withRouter(FacilityPage));
