6d8b8d3eec0387745a69f0fe1003418e91404c51
anibalsolon <anibalsolon@gmail.com>
2018-01-02 11:32:16 -0200

    Setting client as a single project

--- server/src/model/validation.js
+++
@@ -1,7 +0,0 @@
-export const assertNotEmpty = (target, name, message = null) => {
-  if (target == null) throw new Error(message || `Received empty value for required field: ${name}`)
-}
-
-export const assertAny = (target, name, message = null) => {
-  if (target == null || target.length < 1) throw new Error(message || `At least one entry is required for list: ${name}`)
-}
--- server/src/schema/feedback/queries.js
+++
@@ -1,7 +0,0 @@
-export const feedbacks =
-  (obj, { filters }, { models: { Feedbacks: { feedbacks } } }, info) =>
-    feedbacks(filters)
-
-export const feedback =
-  (obj, { _id }, { models: { Feedbacks: { feedback } } }, info) =>
-    feedback(_id)
