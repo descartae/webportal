29a93e3148ec2fb2474e710bbe0549074fc25e16
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-09 13:50:50 -0700

    move language around

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -69,56 +69,46 @@ class Home extends Component {
                 <Typography noWrap variant='headline'>
                   Bem-vindo ao Descartaê!
                 </Typography>
+                <p>Adicione e gerencie dados sobre pontos de coleta de lixo no Brasil.</p>
+                <p>Os dados que você inseriu estão conectados aos aplicativos móveis. Isso ajudará as pessoas a encontrar pontos de coleta próximos e a descobrir informações sobre quais tipos de resíduos são aceitos.</p>
+                <p>Visualizar, adicionar, editar ou remover dados em pontos de coleta: <a className={classes.link} href='/facilities'>Vá ao painel</a> </p>
+                <p className={classes.emphasis}>Obrigado por manter atualizados os dados de seus pontos locais de coleta! <span role="img" aria-label="Feliz">😄</span></p>
+                <p><strong>Adicione pontos de coletas de diversos tipos de resíduos</strong></p>
+                <Paper className={classes.typesOfWastePaper}>
 
-                    <p>Adicione e gerencie dados sobre pontos de coleta de lixo no Brasil.</p>
-                    <p>Os dados que você inseriu estão conectados aos aplicativos móveis. Isso ajudará as pessoas a encontrar pontos de coleta próximos e a descobrir informações sobre quais tipos de resíduos são aceitos.</p>
-                    <p>Visualizar, adicionar, editar ou remover dados em pontos de coleta:</p>
-                    <Button variant="raised" color="primary" className={classes.button}>
-                      <a href='/facilities'>Vá ao painel</a>
-                    </Button>
-                    <p className={classes.emphasis}>Obrigado por manter atualizados os dados de seus pontos locais de coleta! <span role="img" aria-label="Feliz">😄</span></p>
+                {
+                  loading ?
+                    <Loading /> :
+                    (typesOfWaste.map(({ _id, name, icons: { androidMediumURL } }) => (
+                      <Chip
+                        key={_id}
+                        className={classes.typesOfWaste}
+                        avatar={<Avatar src={androidMediumURL} />}
+                        label={name}
+                      />
+                    )))
+                }
 
-                    <Typography noWrap variant='title' gutterBottom>
-                    Por que se preocupar?
-                    </Typography>
+                </Paper>
+                <Typography noWrap variant='title' gutterBottom>
+                Por que se preocupar?
+                </Typography>
 
-                    <p>O mundo está cada vez mais perto de chegar no limte de fornecimento de recursos naturais para produção de novos produtos.</p>
-                    <p>Por isso a importância da reciclagem. Ela reduz significativamente impacto sobre o meio ambiente, diminuindo as retiradas de matéria-prima da natureza, gerando economia de água e energia e reduzindo disposição inadequada do lixo.</p>
+                <p>O mundo está cada vez mais perto de chegar no limte de fornecimento de recursos naturais para produção de novos produtos.</p>
+                <p>Por isso a importância da reciclagem. Ela reduz significativamente impacto sobre o meio ambiente, diminuindo as retiradas de matéria-prima da natureza, gerando economia de água e energia e reduzindo disposição inadequada do lixo.</p>
 
-                    <p className={classes.emphasis}>São geradas 230 mil toneladas de lixo por dia no Brasil. <span role="img" aria-label="Medo">😨</span> <br /> <small>Fonte: Pensamento verde</small></p>
-                    <Typography noWrap variant='title' gutterBottom>
-                    Adicione pontos de coletas de diversos tipos de resíduos
-                    </Typography>
+                <p className={classes.emphasis}>São geradas 230 mil toneladas de lixo por dia no Brasil. <span role="img" aria-label="Medo">😨</span> <br /> <small>Fonte: Pensamento verde</small></p>
+                <Typography noWrap variant='title'>
+                Os bastidores do Projeto
+                </Typography>
+                <Typography noWrap variant='subheading'>
+                Das bibliotecas para mundo <span role="img" aria-label="Livros e Mundo">📚🌎</span>
+                </Typography>
+                <p>A ideia do aplicativo surgiu da iniciativa de um projeto chamado <strong>Feito na Biblioteca</strong>. </p>
+                <p>O projeto visa conectar usuários das bibliotecas públicas da Grande Belo Horizonte e Rio Grande do Sul aos dados abertos do governo para melhorar a vida cotidiana da sociedade.</p>
+                <p>O Descartaê é um projeto open source e sua base de dados é alimentada por bibliotecários vonlutários de cada cidade envolvida </p>
 
-                    <Paper className={classes.typesOfWastePaper}>
-
-                    {
-                      loading ?
-                        <Loading /> :
-                        (typesOfWaste.map(({ _id, name, icons: { androidMediumURL } }) => (
-                          <Chip
-                            key={_id}
-                            className={classes.typesOfWaste}
-                            avatar={<Avatar src={androidMediumURL} />}
-                            label={name}
-                          />
-                        )))
-                    }
-
-                    </Paper>
-
-                    <Typography noWrap variant='title'>
-                    Os bastidores do Projeto
-                    </Typography>
-                    <Typography noWrap variant='subheading'>
-                    Das bibliotecas para mundo <span role="img" aria-label="Livros e Mundo">📚🌎</span>
-                    </Typography>
-
-                    <p>A ideia do aplicativo surgiu da iniciativa de um projeto chamado <strong>Feito na Biblioteca</strong>. </p>
-                    <p>O projeto visa conectar usuários das bibliotecas públicas da Grande Belo Horizonte e Rio Grande do Sul aos dados abertos do governo para melhorar a vida cotidiana da sociedade.</p>
-                    <p>O Descartaê é um projeto open source e sua base de dados é alimentada por bibliotecários vonlutários de cada cidade envolvida </p>
-
-                    <p>Oferecido por: <a className={classes.link} href='http://www.feitonabiblioteca.org/'>Feito na Biblioteca</a> e <a className={classes.link} href='http://caravanstudios.org'>Caravan Studios</a></p>
+                <p>Oferecido por: <a className={classes.link} href='http://www.feitonabiblioteca.org/'>Feito na Biblioteca</a> e <a className={classes.link} href='http://caravanstudios.org'>Caravan Studios</a></p>
               </Grid>
             </Grid>
           </Paper>
