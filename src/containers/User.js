db51d3d454e84bd1b4a2405c5fa786d7b10759c3
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-17 17:01:14 -0700

    update layout - center ocntainers and move New button

--- src/containers/User.js
+++ src/containers/User.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Route, Switch, Link } from 'react-router-dom'
 import { withStyles } from 'material-ui/styles'
-
 import {
   UserDetails,
   UserListing,
@@ -20,6 +19,7 @@ import Unauthorized from '../components/Unauthorized'
 class User extends Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
     match: PropTypes.object.isRequired
   }
 
@@ -28,17 +28,25 @@ class User extends Component {
   }
 
   static styles = theme => ({
-    add: {
-      margin: 0,
-      top: 'auto',
-      right: 20,
-      bottom: 20,
-      left: 'auto',
-      position: 'fixed'
+    root: {
+      overflow: 'hidden',
+      padding: `0 ${theme.spacing.unit * 3}px`,
+    },
+    wrapper: {
+      maxWidth: 1000,
+      margin: 'auto'
     },
     paper: {
-      padding: 16
-    }
+      margin: theme.spacing.unit,
+      padding: theme.spacing.unit * 2,
+    },
+    add: {
+      marginLeft: theme.spacing.unit,
+      float: 'right'
+    },
+    addIcon: {
+      marginRight: theme.spacing.unit,
+    },
   })
 
   render () {
@@ -50,34 +58,34 @@ class User extends Component {
     }
 
     return (
-      <div>
-        <Paper className={classes.paper}>
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography variant='headline'>
-                Usuários
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <UserListing pageSize={5} />
-            </Grid>
-            { !match.isExact ? (
-              <Grid item xs={12} >
-                <Paper className={classes.paper}>
-                  <Switch>
-                    <Route path={`${match.url}/add`} component={UserEditor} />
-                    <Route path={`${match.url}/edit/:userId`} component={UserEditor} />
-                    <Route path={`${match.url}/view/:userId`} component={UserDetails} />
-                  </Switch>
-                </Paper>
+      <div className={classes.root}>
+        <div className={classes.wrapper}>
+          <Paper className={classes.paper}>
+            <Grid container wrap="nowrap" spacing={16}>
+              <Grid item xs size={8}>
+                <Typography variant='headline'>
+                  Usuários
+                <Button variant='fab' mini color='secondary' component={Link} to={`${match.url}/add`} className={classes.add}>
+                  <AddIcon className={classes.addIcon} />
+                </Button>
+                </Typography>
+                <UserListing pageSize={6} />
               </Grid>
-            ) : null }
-          </Grid>
-        </Paper>
+                { !match.isExact ? (
+                  <Grid item xs size={8} >
+                    <Paper className={classes.paper}>
+                      <Switch>
+                        <Route path={`${match.url}/add`} component={UserEditor} />
+                        <Route path={`${match.url}/edit/:userId`} component={UserEditor} />
+                        <Route path={`${match.url}/view/:userId`} component={UserDetails} />
+                      </Switch>
+                    </Paper>
+                  </Grid>
+                ) : null }
+            </Grid> 
+          </Paper>
 
-        <Button variant='fab' color='secondary' component={Link} to={`${match.url}/add`} className={classes.add}>
-          <AddIcon />
-        </Button>
+        </div> 
       </div>
     )
   }
