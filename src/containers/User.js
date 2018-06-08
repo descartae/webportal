6cdaf345cd8b37c38da1737b867514885204c249
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-06-07 19:09:08 -0700

    implement front end user authentication -- if the location user ID and user ID match, user should have access to view/edit their account

--- src/containers/User.js
+++ src/containers/User.js
@@ -50,12 +50,46 @@ class User extends Component {
   })
 
   render () {
-    const { match, classes } = this.props
-    const { auth: { roles } } = this.context
+    const { location, match, classes } = this.props
+    const { auth: { roles} } = this.context
+    // Get the location user ID
+    const path = location.pathname.split("/");
+    const pageUserId = path[path.length-1];
+    // if the logged user ID matches the location user ID, the logged user should have access to their account information/edit their account. Otherwise, only administrators get to have access to all users - create, edit, and view.
 
-    if (roles.indexOf('ADMIN') === -1) {
+    if (roles.indexOf('ADMIN') === -1 && pageUserId === this.context.auth.id) {
+      <div className={classes.root}>
+        <div className={classes.wrapper}>
+          <Paper className={classes.paper}>
+            <Grid container wrap="nowrap" spacing={16}>
+              <Grid item xs size={8}>
+                <Typography variant='headline'>
+                  Usuários
+                <Button variant='fab' mini color='secondary' component={Link} to={`${match.url}/add`} className={classes.add}>
+                  <AddIcon className={classes.addIcon} />
+                </Button>
+                </Typography>
+                <UserListing pageSize={6} />
+              </Grid>
+                { !match.isExact ? (
+                  <Grid item xs size={8} >
+                    <Paper className={classes.paper}>
+                      <Switch>
+                        <Route path={`${match.url}/edit/:userId`} component={UserEditor} />
+                        <Route path={`${match.url}/view/:userId`} component={UserDetails} />
+                      </Switch>
+                    </Paper>
+                  </Grid>
+                ) : null }
+            </Grid>
+          </Paper>
+
+        </div>
+      </div>
+
+    } else if (roles.indexOf('ADMIN') === -1) {
       return (<Unauthorized />)
-    }
+    } 
 
     return (
       <div className={classes.root}>
